7075b1eed2151af0b46c3c0571f89f06dbbba4f3
Richardson Malone <malone0022@gmail.com>
2025-08-24 21:32:53 -0300

    atualizacao do projeto

--- scripts/controller/calccontroller.js
+++ scripts/controller/calccontroller.js
@@ -1,27 +1,29 @@
 class CalcController {
   constructor() {
-    
-    this._displayCalc = "0" / this._dataAtual;
-
+    this._displayCalc = "0";
+    this.currentDate;
+    this.initialize;
   }
+  
+  initialize() {
+
+    let displayCalcEl = document.getElementById("display");
+    let dateEl = document.querySelector("#data");
+    let timeEl = document.querySelector("#hora");
+
+    displayCalcEl.innerHTML = "1234";
+  }
+
   get displayCalc() {
-
     return this._displayCalc;
-
   }
   set displayCalc(value) {
-
     this._displayCalc = valor;
-
   }
   get dataAtual() {
-
-    return this._dataAtual;
-
+    return this.currentDate;
   }
-  set dataAtual(value) {
-
-    this._dataAtual;
-
+  set currentDate(value) {
+    this._currentDate;
   }
 }
